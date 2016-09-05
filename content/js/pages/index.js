feb4ef372909b573eca53bfefb2e9e48a9ff7643
DESKTOP-3OCLHHG\yuanfei <phiree@gmail.com>
2016-09-06 00:44:27 +0800

    dd

--- content/js/pages/index.js
+++ content/js/pages/index.js
@@ -1,1 +1,19 @@
 $('.carousel').bcSwipe({ threshold: 50 });
+<!-- 返回顶部-->
+$(function(){
+
+    $(document).scroll(function() {
+       var scrollDistance=$(document).scrollTop();
+        
+        if(scrollDistance>600)
+       {
+           $("#dvTop").show();
+
+       }
+        else
+       {
+           $("#dvTop").hide();
+       }
+    });
+
+});
